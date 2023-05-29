8f9787ff5ac7a21611fab9ab6b613793ed1a04cf
oreo0228 <t.shun.0228@gmail.com>
2023-05-29 18:05:00 +0900

    test

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var fileInput = document.getElementById('fileInput');
 var grayscaleElement =document.getElementById('grayscale-btn');
 var mosaicElement = document.getElementById('mosaic-btn');
 var binaryElement = document.getElementById('binary-btn');
+var binarySlider = document.getElementById('binary-slider');
+var resetElement = document.getElementById('reset-btn');
+
 const canvas = document.getElementById('canvas');
 
 fileArea.addEventListener('dragover', function(evt){
@@ -19,18 +22,18 @@ fileArea.addEventListener('drop', function(evt){
     evt.preventDefault();
     fileArea.classList.remove('dragenter');
     var files = evt.dataTransfer.files;
-    console.log("DRAG & DROP");
-    console.table(files);
     fileInput.files = files;
-    photoPreview('onChenge',files[0]);
+    photoPreview('onChange',files[0]);
 });
 
+//プレビュー表示
 function photoPreview(event, f = null) {
     var file = f;
     if(file === null){
         file = event.target.files[0];
     }
     var reader = new FileReader();
+    reader.readAsDataURL(file);
     var preview = document.getElementById("preview");
     // ここに画像が入る
     var previewImage = document.getElementById("previewImage");
@@ -38,11 +41,12 @@ function photoPreview(event, f = null) {
     if(previewImage != null) {
         preview.removeChild(previewImage);
     }
+
+    // 画像読み込み完了時に実行
     reader.onload = function(event) {
         var img = document.createElement("img");
         img.setAttribute("src", reader.result);
         img.setAttribute("id", "previewImage");
-        //img.width = "800"
         preview.appendChild(img);
     };
 
@@ -75,18 +79,103 @@ grayscaleElement.addEventListener('click', function() {
     // 変換後の画像をプレビュー表示  
     context.putImageData(imageData, 0, 0);
     img.src = canvas.toDataURL();
+    //console.log(img_url);
 });
+
+function binary(url, k) {
+    const THRESHOLD = k;
+    //var img = document.getElementById('previewImage');
+    var img = new Image();
+    img.src = url;
+    console.log(img.src);
+  
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+  
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    context.drawImage(img, 0, 0, img.width, img.height);
+
+    var imageData = context.getImageData(0, 0, img.width, img.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+    
+        var y = 0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
+        y = parseInt(y, 10);
+        //閾値未満は0、閾値以上は255
+        if (y > THRESHOLD) {
+            y = 255;
+        } else {
+            y = 0;
+        }
+        data[i] = y;
+        data[i + 1] = y;
+        data[i + 2] = y;
+        //data[i + 3] = imageData[i + 3];
+    }
+    context.putImageData(imageData, 0, 0);
+    img.src = canvas.toDataURL();
+    console.log('binary-slider');
+}
 
 //
 //二値化ボタンが押された時
-binaryElement.getElementByID('click', function() {
+binaryElement.addEventListener('click', function() {
+    //閾値
+    const THRESHOLD = 128;
+
+    var img = document.getElementById('previewImage');
+  
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+  
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    context.drawImage(img, 0, 0, img.width, img.height);
+
+    var imageData = context.getImageData(0, 0, img.width, img.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
     
+        var y = 0.2126 * data[i] + 0.7152 * data[i + 1] + 0.0722 * data[i + 2];
+        y = parseInt(y, 10);
+        //閾値未満は0、閾値以上は255
+        if (y > THRESHOLD) {
+            y = 255;
+        } else {
+            y = 0;
+        }
+        data[i] = y;
+        data[i + 1] = y;
+        data[i + 2] = y;
+        //data[i + 3] = imageData[i + 3];
+    }
+    context.putImageData(imageData, 0, 0);
+    img.src = canvas.toDataURL();
+    console.log('binary');
+});
+
+//2値化のスライドバー
+binarySlider.addEventListener('change', function(e) {
+    var value = document.getElementById('binary-slider').value;
+    binary(img_url, Number(value));
+    console.log(value);
 });
 
 //スライドバー
+/*
 document.getElementById('slider').addEventListener('change', function(e) {
     var value = document.getElementById('output1').value;
+
     drawImage(image, Number(value));
+});
+*/
+
+//もとに戻す
+resetElement.addEventListener('click', function(e) {
+    
 });
 
 // 画像を保存ボタンが押された時の処理
