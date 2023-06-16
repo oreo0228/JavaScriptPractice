c2c3a5824dfefd3a36973120e819f602ea79a125
oreo0228 <t.shun.0228@gmail.com>
2023-06-16 17:35:33 +0900

    gg

--- Three.js/index.js
+++ Three.js/index.js
@@ -4,8 +4,8 @@ import { PointerLockControls } from "three/addons/controls/PointerLockControls.j
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 function init() {
-  const width = 1920;
-  const height = 1080;
+  const width = 1900;
+  const height = 920;
 
   // 移動方向の変数宣言
   let moveForward = false;
@@ -31,13 +31,28 @@ function init() {
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
   // カメラの初期座標を設定（X座標:0, Y座標:0, Z座標:0）
-  camera.position.set(4, 4, 2);
-  // カメラコントローラーを作成
-  //const controls = new OrbitControls(camera, canvasElement);
-  // 滑らかにカメラコントローラーを制御する
-  //controls.enableDamping = true;
-  //controls.dampingFactor = 0.2;
+  camera.position.set(4, 3, 2);
+  scene.add(camera);
 
+  // 画面中央に＋を表示
+  const line1 = new THREE.Line(
+    new THREE.BufferGeometry().setFromPoints([
+      new THREE.Vector3(0, -0.01, -1),
+      new THREE.Vector3(0, 0.01, -1),
+    ]),
+    new THREE.MeshNormalMaterial()
+  );
+  line1.raycast = function() {};
+  const line2 = new THREE.Line(
+  new THREE.BufferGeometry().setFromPoints([
+    new THREE.Vector3(-0.01, 0, -1),
+    new THREE.Vector3(0.01, 0, -1),
+  ]),
+  new THREE.MeshNormalMaterial()
+  );
+  line2.raycast = function() {};
+  camera.add(line1);
+  camera.add(line2);
   
   // 一人称視点
   const controls = new PointerLockControls(camera, document.body);
@@ -105,11 +120,11 @@ function init() {
       velocity.x -= velocity.x * 5.0 * delta;
 
       if(moveForward || moveBackward) {
-        velocity.z -= direction.z * 200 * delta;
+        velocity.z -= direction.z * 100 * delta;
       }
 
       if(moveRight || moveLeft) {
-        velocity.x -= direction.x * 200 * delta;
+        velocity.x -= direction.x * 100 * delta;
       }
 
       prevTime = time;
@@ -123,53 +138,60 @@ function init() {
 
   // マウス座標管理用のベクトル
   const mouse = new THREE.Vector2();
+  // レイキャストを作成
+  const raycaster = new THREE.Raycaster();
   // マウスイベント
-  document.addEventListener('mousedown', clickPosition, false);
+  //document.addEventListener('mousedown', clickPosition, false);
+  document.addEventListener('mousedown', generateRaycast, false);
 
-  function clickPosition(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    mouse.x =  ( x / window.innerWidth ) * 2 - 1;
-    mouse.y = -( y / window.innerHeight ) * 2 + 1;
-    // レイキャスト(マウス位置からまっすぐに伸びる光線ベクトルを生成)
+  // 画面中央からレイキャストを生成
+  function generateRaycast(e) {
+    const center = new THREE.Vector3(0, 0, 0);
+    /*
+    // カメラの視点（中心位置）を取得
+    const cameraCenter = new THREE.Vector3();
+    camera.getWorldPosition(cameraCenter);
+
+    // マウスの位置を正規化デバイス座標系（Normalized Device Coordinates, NDC）に変換
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    // マウス位置のベクトルを3D空間に変換
+    const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, camera);
-    // その光線とぶつかったオブジェクトを得る
-    const intersects = raycaster.intersectObjects(scene.children);
-    // ぶつかったオブジェクトを
+    */
+
+    // カメラの中心位置からレイキャストを生成
+    const ray = new THREE.Raycaster();
+    ray.setFromCamera(center, camera);
+    //const ray = new THREE.Raycaster(center, camera);
+
+    // レイキャストにぶつかったオブジェクトを得る
+    const intersects = ray.intersectObjects(scene.children);
+
     if(intersects.length > 0) {
-      console.log(intersects[0].distance);
+      //console.log(intersects[0]);
+      console.log(intersects[0].normal);
+      console.log(intersects[0].point.x);
+      console.log(intersects[0]);
     }
-  }
-
-  // マウスを動かしたときのイベント
-  function handleMouseMove(event) {
-    const element = event.currentTarget;
-    // canvas要素上のXY座標
-    const x = event.clientX - element.offsetLeft;
-    const y = event.clientY - element.offsetTop;
-    // canvas要素の幅・高さ
-    const w = element.offsetWidth;
-    const h = element.offsetHeight;
-
-    // -1〜+1の範囲で現在のマウス座標を登録する
-    mouse.x = ( x / w ) * 2 - 1;
-    mouse.y = -( y / h ) * 2 + 1;
-    console.log(mouse.x, mouse.y);
+    // レイキャストの結果を返す（必要に応じて処理を実行）
+    return ray;
   }
   
   // 背景色
   scene.background = new THREE.Color( 0x00CCFF );
 
   // 地面を作成
-  let a = 50;
-  let b = 50;
+  let a = 1;
+  let b = 1;
   for (let i = 0; i < 50; i++) {
     for (let j = 0; j < 50; j++) {
-      createGround(a, 1, b);
-      a -= 2;
+      createGround(a, -1, b);
+      a += 2;
     }
-    a = 50
-    b -= 2;
+    a = 1
+    b += 2;
   }
 
   // 地面ブロックの作成
@@ -183,11 +205,11 @@ function init() {
     scene.add(cube);
   }
 
-  createTree(0, 1, 0, 5);
+  createTree(5, 1, 5, 5);
 
   // 木の作成
   function createTree(x, y, z, h) {
-    for (let i = 0; i <= h; i++, y += 2) {
+    for (let i = 0; i < h; i++, y += 2) {
       const texture = new THREE.TextureLoader().load('images/wood.png');
       const groundGeometry = new THREE.BoxGeometry(2, 2, 2);
       const groundMaterial = new THREE.MeshPhongMaterial({map: texture});
@@ -196,6 +218,44 @@ function init() {
       cube.name = 'Tree';
       scene.add(cube);
     }
+  }
+
+  const putObject = (e) =>{
+    console.log('putObject');
+    if (e.code === "KeyG") {
+      const ray = generateRaycast();
+      const pointX = Math.floor(ray.position.x);
+      const pointY = Math.floor(ray.position.y);
+      const pointZ = Math.floor(ray.position.z);
+      if (pointX % 2 === 0) {
+        pointX += 1;
+      }
+      if (pointY % 2 === 0) {
+        pointX += 1;
+      }
+      if (pointZ % 2 === 0) {
+        pointX += 1;
+      }
+      const normal = ray.normal;
+
+      pointX += normal.x * 2;
+      pointY += normal.y * 2;
+      pointZ += normal.z * 2;
+    
+      putGround(pointX, pointY, pointZ);
+      console.log("Put!");
+    }  
+  }
+  document.addEventListener('keydown', putObject);
+
+  function putGround(x, y, z) {
+    const texture = new THREE.TextureLoader().load('images/Ground.png');
+    const groundGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const groundMaterial = new THREE.MeshPhongMaterial({map: texture});
+    const cube = new THREE.Mesh(groundGeometry, groundMaterial);
+    cube.position.set(x, y, z);
+    cube.name = 'Ground';
+    scene.add(cube);
   }
 
   // 座標軸を追加
@@ -209,15 +269,15 @@ function init() {
   // シーンに追加
   scene.add(light);
 
+  /*
   // 画面の中央に黒い点を表示するためのマテリアルとジオメトリを作成
   const pointMaterial = new THREE.PointsMaterial({ color: 0x000000 });
-  const pointGeometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, -5)]); // カメラの前方に配置
+  // カメラの前方に配置
+  const pointGeometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, -5)]);
   // ポイントオブジェクトを作成し、シーンに追加
   const pointObject = new THREE.Points(pointGeometry, pointMaterial);
   camera.add(pointObject);
-
-  // レイキャストを作成
-  const raycaster = new THREE.Raycaster();
+  */
 
   // 初回実行
   tick();
